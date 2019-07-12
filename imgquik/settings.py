61326489a519a01f63a32ae1ed76b0616fde4fac
khan458sheridan <khan458@sheridancollege.ca>
2019-07-12 00:20:27 -0400

    add whitenoise

--- imgquik/settings.py
+++ imgquik/settings.py
@@ -25,6 +25,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'imgquik.urls'
@@ -86,3 +87,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
